d48f1d1d2bd70e39d843d92e470ac3ceb17c1213
Summer Ray <shunshynne@gmail.com>
2024-01-25 14:07:02 -0500

    stuff

--- getinfo.py
+++ getinfo.py
@@ -4,6 +4,8 @@ import requests
 import certifi
 import json
 
+# we need to open the authorization file to get the login name, password and token 
+# we keep it in a separate file for security 
 with open("AUTH.json", "r") as file:
 
     data = json.load(file)
@@ -12,18 +14,20 @@ with open("AUTH.json", "r") as file:
     password = data["password"]
     api_key = data["api_key"]
 
+
 headers ={'Content-Type': 'application/json', 'Accept': 'application/json'}
 data = {'login_id': login_id, 'password': password, 'api_key': api_key}
 
 session = requests.session()
 response = session.post('https://api.breadware.com/v2/login', data=json.dumps(data), headers=headers)
 
-url = "https://api.breadware.com/v2/events/shipment_info/automation"
+url = "https://api.breadware.com/v2/events/shipment_info/173574255486455"
 params = {
 	"since":"2020-01-01T14:16:26-05:00", 
-	"until":"2024-01-24T14:16:26-05:00", 
-	"limit":100 
+	"until":"2024-01-24T14:16:26-05:00",
+    "sort_direction": "asc",
+    "sort_by": "received_at"
 }
 
-# GET
 response = session.get(url=url, params=params, headers=headers, verify=certifi.where())
+print(response.content)
